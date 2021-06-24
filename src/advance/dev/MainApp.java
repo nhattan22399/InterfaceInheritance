e21f914b2b8df60527a2c73b7ffc12a0f4be8afd
nhattan22399 <linhatan99@gmail.com>
2021-06-25 02:35:20 +0700

    first commit

---
+++ src/advance/dev/MainApp.java
@@ -0,0 +1,14 @@
+package advance.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ItStudent std = new ItStudent();
+		std.code();
+		std.hackFacebook();
+		std.playGame();
+		std.sleep(null);
+	}
+
+}
